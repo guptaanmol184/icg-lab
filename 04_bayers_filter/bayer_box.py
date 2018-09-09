20ffb2dcca75afd33f0f14852c7ac6f9ce3cf40f
Anmol Gupta <guptaanmol184@gmail.com>
2018-09-09 08:22:43 +0530

    Demosaicing

--- 04_bayers_filter/bayer_box.py
+++ 04_bayers_filter/bayer_box.py
@@ -36,4 +36,58 @@ for i in range(height):
 # save the random image
 Image.fromarray(img_box, 'RGB').save('rgb.png')
 
+# remove the edges
+img_demosaic = img[1:-1, 1:-1]
+new_height , new_width = img_demosaic.shape
+img_demosaic_box = np.zeros((new_height * box_size, new_width * box_size, 3), dtype=np.uint8)
+
 # do demosaicing
+for i in range(new_height):
+    for j in range(new_width):
+        if i%2 == 0 and j%2 == 0:
+            # 1st row and 1st column of original = B
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    0] = np.uint8((img[i-1, j-1] + img[i-1, j+1] + img[i+1, j-1] + img[i+1, j+1])/4)
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    1] = np.uint8((img[i-1, j] + img[i, j-1] + img[i, j+1] + img[i+1, j])/4)
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    2] = img[i, j]
+        elif i%2 == 0 and j%2 == 1:
+            # 1st row and 2nd column of original = G
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    0] = np.uint8((img[i-1, j] + img[i+1, j])/2)
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    1] = img[i, j]
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    2] = np.uint8((img[i, j-1] + img[i, j+1])/2)
+        elif i%2 == 1 and j%2 == 0:
+            # 2nd row and 1st column of original = G
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    0] = np.uint8((img[i, j-1] + img[i, j+1])/2)
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    1] = img[i, j]
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    2] = np.uint8((img[i-1, j] + img[i+1, j])/2)
+        elif i%2 == 1 and j%2 == 1:
+            # 2nd row and 2nd column of original = R
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    0] = img[i, j]
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    1] = np.uint8((img[i-1, j] + img[i+1, j] + img[i, j-1] + img[i, j+1])/4)
+            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
+                    j*box_size: j*box_size+(box_size-1),
+                    2] = np.uint8((img[i-1, j-1] + img[i-1, j+1] + img[i+1, j-1] + img[i+1, j+1])/4)
+
+# save demosaiced image
+Image.fromarray(img_demosaic_box, 'RGB').save('demosaic.png')
