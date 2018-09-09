1e715f4c8f385ee8ee05be049ec4769fd0a2235c
Anmol Gupta <guptaanmol184@gmail.com>
2018-09-09 15:23:03 +0530

    Rewrite - correct mistake

--- 04_bayers_filter/bayer_box.py
+++ 04_bayers_filter/bayer_box.py
@@ -1,97 +1,85 @@
 #!/usr/bin/env python
 
 import numpy as np
+from random import randint
 from PIL import Image
+
+def print3darray(array):
+    for array_row in array:
+        print(array_row.tolist())
+
+def convert_to_box(img, box_size):
+    height, width, temp = img.shape
+    img_box = np.zeros((height*box_size, width*box_size, 3), dtype=np.uint8)
+    for i in range(height):
+        for j in range(width):
+                img_box[i*box_size: i*box_size+(box_size),
+                        j*box_size: j*box_size+(box_size)] = img[i, j]
+    return img_box
 
 # generate a random image
 #73 128
-height = 10
-width = 10
-box_size = 50
-img = np.random.randint(0, 256, (height, width), np.uint8)
-img_box = np.zeros((height * box_size, width * box_size, 3), dtype=np.uint8)
-# generate img_box from img
+height = 18
+width = 32
+box_size = 20
+print('Height: {}, Width: {}, Box Size: {}'.format(height, width, box_size))
+img = np.zeros((height, width, 3), dtype=np.uint8)
+
 for i in range(height):
     for j in range(width):
-        if i%2 == 0 and j%2 == 0:
-            # Oth row and 0th column == R
-            img_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    0] = img[i, j]
-        elif i%2 == 0 and j%2 == 1:
-            # 0th row and 1st column == G
-            img_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    1] = img[i, j]
-        elif i%2 == 1 and j%2 == 0:
-            # 1st row and 0th column == G
-            img_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    1] = img[i, j]
-        elif i%2 == 1 and j%2 == 1:
-            # 1st row and 1st column == B
-            img_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    2] = img[i, j]
+        if i%2 == 0:
+            if j%2 == 0:
+                img[i, j, 0] = randint(0, 255)
+            else:
+                img[i, j, 1] = randint(0, 255)
+        else:
+            if j%2 == 0:
+                img[i, j, 1] = randint(0, 255)
+            else:
+                img[i, j, 2] = randint(0, 255)
 
-# remove edge rows for display
-img_box_noedge = img_box[box_size:-box_size, box_size:-box_size, :]
+
+# print the updated box
+print('bayer filtered image captured:')
+print3darray(img)
+img_box = convert_to_box(img, box_size)
+# print3darray(img_box)
 # save the random image
-Image.fromarray(img_box, 'RGB').save('rgb_edge.png')
-Image.fromarray(img_box_noedge, 'RGB').save('rgb.png')
+# Image.fromarray(img, 'RGB').save('rgb.png')
+Image.fromarray(img_box, 'RGB').save('rgb_box.png')
 
-# remove the edges
-img_demosaic = img[1:-1, 1:-1]
-new_height , new_width = img_demosaic.shape
-img_demosaic_box = np.zeros((new_height * box_size, new_width * box_size, 3), dtype=np.uint8)
-
-# do demosaicing
-for i in range(new_height):
-    for j in range(new_width):
-        if i%2 == 0 and j%2 == 0:
-            # 1st row and 1st column of original = B
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    0] = np.uint8((img[i-1, j-1] + img[i-1, j+1] + img[i+1, j-1] + img[i+1, j+1])/4)
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    1] = np.uint8((img[i-1, j] + img[i, j-1] + img[i, j+1] + img[i+1, j])/4)
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    2] = img[i, j]
-        elif i%2 == 0 and j%2 == 1:
-            # 1st row and 2nd column of original = G
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    0] = np.uint8((img[i-1, j] + img[i+1, j])/2)
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    1] = img[i, j]
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    2] = np.uint8((img[i, j-1] + img[i, j+1])/2)
-        elif i%2 == 1 and j%2 == 0:
-            # 2nd row and 1st column of original = G
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    0] = np.uint8((img[i, j-1] + img[i, j+1])/2)
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    1] = img[i, j]
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    2] = np.uint8((img[i-1, j] + img[i+1, j])/2)
-        elif i%2 == 1 and j%2 == 1:
-            # 2nd row and 2nd column of original = R
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    0] = img[i, j]
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    1] = np.uint8((img[i-1, j] + img[i+1, j] + img[i, j-1] + img[i, j+1])/4)
-            img_demosaic_box[i*box_size: i*box_size+(box_size-1),
-                    j*box_size: j*box_size+(box_size-1),
-                    2] = np.uint8((img[i-1, j-1] + img[i-1, j+1] + img[i+1, j-1] + img[i+1, j+1])/4)
+# DEMOSAIC
+img_demosaic = np.zeros(img.shape, dtype=np.uint8)
+# we go from 1 to height-2
+# since we leave off the edges
+# have to use np.uint16 because 8 bit leads to overflow
+for i in range(1, height-1):
+    for j in range(1, width-1):
+        if i%2 == 0:
+            if j%2 == 0:
+                # ceter is red
+                r = img[i, j]
+                g = (np.uint16(img[i-1, j]) + np.uint16(img[i+1, j]) + np.uint16(img[i, j-1]) + np.uint16(img[i, j+1]))/4
+                b = (np.uint16(img[i-1, j-1]) + np.uint16(img[i-1, j+1]) + np.uint16(img[i+1, j-1]) + np.uint16(img[i+1, j+1]))/4
+            else:
+                # center is green
+                r = (np.uint16(img[i, j-1]) + np.uint16(img[i, j+1]))/2
+                g = img[i, j]
+                b = (np.uint16(img[i-1, j]) + np.uint16(img[i+1, j]))/2
+        else:
+            if j%2 == 0:
+                # centre is green
+                r = (np.uint16(img[i-1, j]) + np.uint16(img[i+1, j]))/2
+                g = img[i, j]
+                b = (np.uint16(img[i, j-1]) + np.uint16(img[i, j+1]))/2
+            else:
+                # centre is blue
+                r = (np.uint16(img[i-1, j-1]) + np.uint16(img[i-1, j+1]) + np.uint16(img[i+1, j-1]) + np.uint16(img[i+1, j+1]))/4
+                g = (np.uint16(img[i-1, j]) + np.uint16(img[i+1, j]) + np.uint16(img[i, j-1]) + np.uint16(img[i, j+1]))/4
+                b = img[i, j]
+        img_demosaic[i, j] = r + g + b
 
 # save demosaiced image
-Image.fromarray(img_demosaic_box, 'RGB').save('demosaic.png')
+print3darray(img_demosaic)
+img_demosaic_box = convert_to_box(img_demosaic, box_size)
+Image.fromarray(img_demosaic_box, 'RGB').save('demosaic_box.png')
